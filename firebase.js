809b26db956b62cc956d66f60752924240793e5d
Taha Haza <76169267+TahaHaza@users.noreply.github.com>
2021-05-22 16:46:55 +0100

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,10 +1,10 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyAdg6r9WpEnnBOBKAe5iUbZuX6qGpNBPZc",
-    authDomain: "fishbn-f7381.firebaseapp.com",
-    databaseURL: "https://fishbn-f7381-default-rtdb.europe-west1.firebasedatabase.app",
-    projectId: "fishbn-f7381",
-    storageBucket: "fishbn-f7381.appspot.com",
-    messagingSenderId: "1058190624520",
-    appId: "1:1058190624520:web:f3323c7065bbb406a8cebd",
-    measurementId: "G-84KG7R77CC"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
   };
